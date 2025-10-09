cdd5176ed1b72eb1b9b580be62c29a1ed99e2674
Alexander Jackson <ajMobileConsulting@gmail.com>
2025-10-09 04:55:05 -0400

    storage upload failed in production changing allow to identity → authenticated

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -5,7 +5,7 @@ export const storage = defineStorage({
   access: (allow) => ({
     'user-videos/{identity_id}/*': [
       allow.guest.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
+      allow.authenticated.to(['read', 'write', 'delete'])
     ]
   })
 });
